18abdeb4cd835478f5a7d7797ea453ed49dd40ab
Dinesh Reddy <dineshreddyboddapati9@gmail.com>
2026-09-20 15:22:03 +0530

    Add Freshworks to SmartRecruiters ingestion

--- lib/ingestion/smartrecruiters-companies.ts
+++ lib/ingestion/smartrecruiters-companies.ts
@@ -8,4 +8,8 @@ export const SMARTRECRUITERS_COMPANIES: SmartRecruitersCompanyConfig[] = [
     identifier: "PHONEPELIMITED",
     companyName: "PhonePe Limited",
   },
+  {
+    identifier: "Freshworks",
+    companyName: "Freshworks",
+  },
 ];
